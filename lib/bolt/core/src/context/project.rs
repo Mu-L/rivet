4a65d874cb6f6594738cef39b3f7d845a805b218
MasterPtato <maxpopkov.contact@gmail.com>
2023-12-15 00:47:05 +0000

    Write cluster worker unit tests

--- lib/bolt/core/src/context/project.rs
+++ lib/bolt/core/src/context/project.rs
@@ -180,12 +180,14 @@ impl ProjectContextData {
 				);
 				unique_datacenter_ids.insert(datacenter.datacenter_id);
 
-				let ats_count = datacenter
+				let Some(ats_pool) = datacenter
 					.pools
 					.get(&config::ns::DynamicServersDatacenterPoolType::Ats)
-					.map(|pool| pool.desired_count)
-					.unwrap_or_default();
+				else {
+					panic!("invalid datacenter ({}): Missing ATS pool", name_id);
+				};
 
+				let ats_count = ats_pool.desired_count;
 				match datacenter.build_delivery_method {
 					config::ns::DynamicServersBuildDeliveryMethod::TrafficServer => {
 						assert_ne!(
@@ -211,7 +213,7 @@ impl ProjectContextData {
 					.unwrap_or_default();
 				assert_ne!(
 					gg_count, 0,
-					"invalid datacenter ({}): missing gg servers",
+					"invalid datacenter ({}): Missing GG servers",
 					name_id,
 				);
 				let job_count = datacenter
@@ -222,7 +224,7 @@ impl ProjectContextData {
 
 				assert_ne!(
 					job_count, 0,
-					"invalid datacenter ({}): missing job servers",
+					"invalid datacenter ({}): Missing job servers",
 					name_id,
 				);
 			}
